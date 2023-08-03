da063ab0ffd162e1e03662a17d172d0c3f31045b
Royston Sim <roys.exe@gmail.com>
2023-08-03 19:49:45 +0800

    Completed 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(repeat, num) {
+    if (Math.sign(num) == 1 || Math.sign(num) == 0) {
+        return repeat.repeat(num)
+    } else {
+        return "ERROR"
+    }
+    
 };
 
 // Do not edit below this line
