79c2fab8be55fabee624c942b17e1016eb5d89de
Royston Sim <roys.exe@gmail.com>
2023-08-12 14:45:58 +0800

    Completed up to 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let modifiedString = word.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reversedModifiedString = modifiedString.split("").reverse().join("");
 
+    if (modifiedString == reversedModifiedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    //1, 2, 3, 4, 5, 6, 7,  8,  9,  10, 11, 12
+    //0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144.
 
+    // function(6)
+    if (n < 0) {
+        return "OOPS"
+        
+    } else {
+
+        const arr = [0, 1]
+        for (i = 2; i < n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2]
+        }
+        return (arr[arr.length - 1] + arr[arr.length - 2])
+
+    }
 };
 
 // Do not edit below this line
