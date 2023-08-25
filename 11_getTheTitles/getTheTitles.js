846328438faa5621df7f8b3c89f7dc66f4591cfb
Royston Sim <roys.exe@gmail.com>
2023-08-25 16:40:33 +0800

    completed 11 and 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,18 @@
-const getTheTitles = function() {
+// const getTheTitles = function(arrayDataHere) {
+//     let mappedOutArray = arrayDataHere.map(
+//         (book) => book.title)
+//     return mappedOutArray;
+// };
 
+const getTheTitles = function(arrayDataHere) {
+
+    let findBookTitles = function(book){ 
+        return book.title
+    }
+
+    let mappedOutArray = arrayDataHere.map(findBookTitles) // ['Book', 'Book2']
+
+    return mappedOutArray
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayData) {
+  /*
+  0 - Create a function to find out how old someone was before they died
+  1 - Find out how old everyone is by taking yearOfDeath minus yearOfBirth
+  2 - Compare the results. name with the biggest number is the oldest, return it
+  */
+ return arrayData.reduce((oldestPerson, currentPerson) => {
+  let oldestAge = yearsAlive(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath)
+  let currentAge = yearsAlive(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
 
-};
+  if (oldestAge < currentAge) {
+    return currentPerson
+  }
+    else {
+      return oldestPerson
+    }
+  })
+ }
 
+
+
+const yearsAlive = function(birthYear, deathYear) {
+  if (deathYear == null) {
+    deathYear = new Date().getFullYear();
+  }
+  return deathYear - birthYear
+}
 // Do not edit below this line
 module.exports = findTheOldest;
